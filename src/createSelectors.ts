54856f956409d123fd3c9a8d9c68a38198f29ef5
no <no>
2020-12-07 17:25:33 +0200

    init

---
+++ src/createSelectors.ts
@@ -0,0 +1,52 @@
+type Slice<ST, SL extends keyof ST> = ST[SL];
+type Selector<ST, R = any> = (state: ST) => R;
+type SelectorsMap<ST> = Record<string, Selector<ST>>;
+type BoundSelectorsMap<SM extends SelectorsMap<any>, ST> = {
+    [S in keyof SM]: Selector<ST, ReturnType<SM[S]>>;
+};
+type SelectorsSliceMap<ST> = {
+    [SL in keyof ST]: SelectorsMap<Slice<ST, SL>>;
+};
+type BoundSelectorsSliceMap<SSM extends SelectorsSliceMap<ST>, ST> = {
+    [SL in keyof ST]: BoundSelectorsMap<SSM[SL], ST>;
+};
+
+const bindSelectorToSlice = <
+    ST extends Record<string, any>,
+    SL extends keyof ST,
+    SEL extends Selector<Slice<ST, SL>>
+>(
+    slice: SL,
+    selector: SEL
+) => (state: ST): ReturnType<typeof selector> => selector(state[slice]);
+
+function bindSelectorsMapToSlice<
+    ST,
+    SL extends keyof ST,
+    SELMAP extends SelectorsMap<Slice<ST, SL>>
+>(slice: SL, selectorsMap: SELMAP): BoundSelectorsMap<SELMAP, ST> {
+    return Object.entries(selectorsMap).reduce(
+        (boundSelectorsMap, [selectorName, selector]) => {
+            boundSelectorsMap[
+                selectorName as keyof SELMAP
+            ] = (bindSelectorToSlice as any)(slice, selector);
+            return boundSelectorsMap;
+        },
+        ({} as unknown) as BoundSelectorsMap<SELMAP, ST>
+    );
+}
+
+export function combineSelectors<
+    ST extends Record<string, any>,
+    SSM extends SelectorsSliceMap<ST>
+>(slicedSelectorsMap: SSM): BoundSelectorsSliceMap<SSM, ST> {
+    return Object.entries(slicedSelectorsMap).reduce(
+        (combinedSelectors, [sliceName, selMap]) => {
+            combinedSelectors[
+                sliceName as keyof ST
+            ] = (bindSelectorsMapToSlice as any)(sliceName, selMap);
+            return combinedSelectors;
+        },
+        ({} as unknown) as BoundSelectorsSliceMap<SSM, ST>
+    );
+}
---
+++ src/examples/example.ts
@@ -0,0 +1,48 @@
+import { combineSelectors } from '../createSelectors';
+
+interface Todo {
+    title: string;
+    isComplete: string;
+}
+
+type TodoState = {
+    todos: Todo[];
+};
+
+// =
+
+interface User {
+    displayName: string;
+    email: string;
+}
+
+interface UserState {
+    users: User[];
+}
+
+// =
+
+interface State {
+    todo: TodoState;
+    users: UserState;
+}
+
+// ---
+
+const state = ({} as unknown) as State;
+
+const selectFirstTodo = (state: TodoState): Todo | undefined => state.todos[0];
+const selectFirstUser = (state: UserState): User | undefined => state.users[0];
+
+const ssMap = {
+    todo: {
+        selectFirstTodo,
+    },
+    // users: {
+    //     selectFirstUser,
+    // },
+};
+
+const combibedSelectors = combineSelectors<State, typeof ssMap>(ssMap);
+
+combibedSelectors.todo.selectFirstTodo(state)?.isComplete;
